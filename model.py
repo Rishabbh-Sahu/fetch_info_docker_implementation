ce759451519ab0847ec0d758eb3202ee93004bb2
Rishabbh <69572197+Rishabbh-Sahu@users.noreply.github.com>
2022-05-12 20:21:41 +0100

    docstring added

--- model.py
+++ model.py
@@ -5,8 +5,12 @@ Created on Wed Mar 30 2022
 """
 import re
 
-# Dacorator definition to log in text to be processed
+
 def logging_info(orig_func):
+    """
+    Define dacorator which is used to log-in text info going to be 
+    processed everytime this routine is called 
+    """
     import logging
     logging.basicConfig(filename='{}.log'.format(orig_func.__name__), level=logging.INFO)
 
